adea22e3e631fbbd4522b9f45e9c36feb831ef82
wdh970616 <wdh970616@naver.com>
2024-08-05 16:48:13 +0900

    [feature]
    * havis 데이터베이스 설정 sql문 수정

--- src/main/resources/sql/setHavisDB.sql
+++ src/main/resources/sql/setHavisDB.sql
@@ -1,3 +1,3 @@
 create user if not exists 'havis'@'%' identified by 'havis';
 create database if not exists havisdb;
-grant all privileges  on menudb.* to 'havis'@'%';
+grant all privileges  on havisdb.* to 'havis'@'%';
